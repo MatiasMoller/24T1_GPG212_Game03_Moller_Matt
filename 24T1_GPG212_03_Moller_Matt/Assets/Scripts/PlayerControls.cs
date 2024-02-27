cc39a97f722c2dd945d82cc7cc9d0f3e8f4c1d18
Moller_Matt_Laptop <136556009+MatiasMoller@users.noreply.github.com>
2024-02-27 12:22:55 +1100

    Added spikes and walls and more platforms

--- 24T1_GPG212_03_Moller_Matt/Assets/Scripts/PlayerControls.cs
+++ 24T1_GPG212_03_Moller_Matt/Assets/Scripts/PlayerControls.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerControls : MonoBehaviour
 {
@@ -9,21 +10,42 @@ public class PlayerControls : MonoBehaviour
     public float groundCheckRadius;
     public LayerMask whatIsGround;
     private bool onGround;
+    private bool forward;
+
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        forward = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        rb.velocity = new Vector2(4, rb.velocity.y);
+        // Adjust velocity based on the forward variable
+        float moveSpeed = forward ? 6f : -6f;
+        rb.velocity = new Vector2(moveSpeed, rb.velocity.y);
+
         onGround = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, whatIsGround);
 
         if (Input.GetMouseButtonDown(0) && onGround)
         {
-            rb.velocity = new Vector2(rb.velocity.x, 3);
+            rb.velocity = new Vector2(rb.velocity.x, 5);
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        // Check if the collided object has the tag "Wall"
+        if (collision.gameObject.CompareTag("Wall"))
+        {
+            // Reverse the forward variable and adjust velocity accordingly
+            forward = !forward;
+            rb.velocity = new Vector2(forward ? 6f : -6f, rb.velocity.y);
+        }
+        if (collision.gameObject.CompareTag("Spike"))
+        {
+            SceneManager.LoadScene("Game");
         }
     }
 }
